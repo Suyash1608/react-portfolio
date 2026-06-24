af12e61d9257b26f4b9dc9a8fa888bf500baf883
Suyash Gupta <suyashgupta1608@gmail.com>
2026-06-24 13:18:54 +0530

    Configure Vite and routing for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,29 @@ import About from './Components/About/About'
 import Education from './Components/About/Education'
 import Experience from './Components/About/Experience'
 import Certification from './Components/About/Certification'
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom' // Changed BrowserRouter to HashRouter
+
 function App() {
   return (
     <div>
-      <BrowserRouter>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element = {<HomePage></HomePage>}></Route>
-        <Route path='/about' element = {<AboutRender></AboutRender>}>
-          <Route path='' element ={<About></About>}></Route>
-          <Route path='education' element = {<Education></Education>}></Route>
-          <Route path='experience' element = {<Experience></Experience>}></Route>
-          <Route path='certifications' element = {<Certification></Certification>}></Route>
-        </Route>
-        <Route path='/projects' element = {<Projects></Projects>}></Route>
-        <Route path='/skills' element = {<Skills></Skills>}></Route>
-        <Route path='/contact' element = {<Contact></Contact>}></Route>
-      </Routes>
-      </BrowserRouter>
+      <HashRouter> {/* Changed to HashRouter */}
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<HomePage></HomePage>}></Route>
+          <Route path='/about' element={<AboutRender></AboutRender>}>
+            <Route path='' element={<About></About>}></Route>
+            <Route path='education' element={<Education></Education>}></Route>
+            <Route path='experience' element={<Experience></Experience>}></Route>
+            <Route path='certifications' element={<Certification></Certification>}></Route>
+          </Route>
+          <Route path='/projects' element={<Projects></Projects>}></Route>
+          <Route path='/skills' element={<Skills></Skills>}></Route>
+          <Route path='/contact' element={<Contact></Contact>}></Route>
+        </Routes>
+      </HashRouter> {/* Changed to HashRouter */}
     </div>
   )
 }
 
 export default App
+
